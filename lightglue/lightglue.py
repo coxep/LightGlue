9ebfef2a176c2d3efe3e62de381a7ba447f4f8e3
Eric Cox <cox_eric@bah.com>
2024-01-15 17:58:02 -0500

    WIP

--- lightglue/lightglue.py
+++ lightglue/lightglue.py
@@ -8,16 +8,6 @@ import torch
 import torch.nn.functional as F
 from torch import nn
 
-try:
-    from flash_attn.modules.mha import FlashCrossAttention
-except ModuleNotFoundError:
-    FlashCrossAttention = None
-
-if FlashCrossAttention or hasattr(F, "scaled_dot_product_attention"):
-    FLASH_AVAILABLE = True
-else:
-    FLASH_AVAILABLE = False
-
 torch.backends.cudnn.deterministic = True
 
 
@@ -88,49 +78,21 @@ class TokenConfidence(nn.Module):
 
 
 class Attention(nn.Module):
-    def __init__(self, allow_flash: bool) -> None:
+    def __init__(self) -> None:
         super().__init__()
-        if allow_flash and not FLASH_AVAILABLE:
-            warnings.warn(
-                "FlashAttention is not available. For optimal speed, "
-                "consider installing torch >= 2.0 or flash-attn.",
-                stacklevel=2,
-            )
-        self.enable_flash = allow_flash and FLASH_AVAILABLE
-        self.has_sdp = hasattr(F, "scaled_dot_product_attention")
-        if allow_flash and FlashCrossAttention:
-            self.flash_ = FlashCrossAttention()
-        if self.has_sdp:
-            torch.backends.cuda.enable_flash_sdp(allow_flash)
 
     def forward(self, q, k, v, mask: Optional[torch.Tensor] = None) -> torch.Tensor:
-        if self.enable_flash and q.device.type == "cuda":
-            # use torch 2.0 scaled_dot_product_attention with flash
-            if self.has_sdp:
-                args = [x.half().contiguous() for x in [q, k, v]]
-                v = F.scaled_dot_product_attention(*args, attn_mask=mask).to(q.dtype)
-                return v if mask is None else v.nan_to_num()
-            else:
-                assert mask is None
-                q, k, v = [x.transpose(-2, -3).contiguous() for x in [q, k, v]]
-                m = self.flash_(q.half(), torch.stack([k, v], 2).half())
-                return m.transpose(-2, -3).to(q.dtype).clone()
-        elif self.has_sdp:
-            args = [x.contiguous() for x in [q, k, v]]
-            v = F.scaled_dot_product_attention(*args, attn_mask=mask)
-            return v if mask is None else v.nan_to_num()
-        else:
-            s = q.shape[-1] ** -0.5
-            sim = torch.einsum("...id,...jd->...ij", q, k) * s
-            if mask is not None:
-                sim.masked_fill(~mask, -float("inf"))
-            attn = F.softmax(sim, -1)
-            return torch.einsum("...ij,...jd->...id", attn, v)
+        s = q.shape[-1] ** -0.5
+        sim = torch.einsum("...id,...jd->...ij", q, k) * s
+        if mask is not None:
+            sim.masked_fill(~mask, -float("inf"))
+        attn = F.softmax(sim, -1)
+        return torch.einsum("...ij,...jd->...id", attn, v)
 
 
 class SelfBlock(nn.Module):
     def __init__(
-        self, embed_dim: int, num_heads: int, flash: bool = False, bias: bool = True
+        self, embed_dim: int, num_heads: int, bias: bool = True
     ) -> None:
         super().__init__()
         self.embed_dim = embed_dim
@@ -138,7 +100,7 @@ class SelfBlock(nn.Module):
         assert self.embed_dim % num_heads == 0
         self.head_dim = self.embed_dim // num_heads
         self.Wqkv = nn.Linear(embed_dim, 3 * embed_dim, bias=bias)
-        self.inner_attn = Attention(flash)
+        self.inner_attn = Attention()
         self.out_proj = nn.Linear(embed_dim, embed_dim, bias=bias)
         self.ffn = nn.Sequential(
             nn.Linear(2 * embed_dim, 2 * embed_dim),
@@ -165,7 +127,7 @@ class SelfBlock(nn.Module):
 
 class CrossBlock(nn.Module):
     def __init__(
-        self, embed_dim: int, num_heads: int, flash: bool = False, bias: bool = True
+        self, embed_dim: int, num_heads: int, bias: bool = True
     ) -> None:
         super().__init__()
         self.heads = num_heads
@@ -181,10 +143,6 @@ class CrossBlock(nn.Module):
             nn.GELU(),
             nn.Linear(2 * embed_dim, embed_dim),
         )
-        if flash and FLASH_AVAILABLE:
-            self.flash = Attention(True)
-        else:
-            self.flash = None
 
     def map_(self, func: Callable, x0: torch.Tensor, x1: torch.Tensor):
         return func(x0), func(x1)
@@ -198,22 +156,16 @@ class CrossBlock(nn.Module):
             lambda t: t.unflatten(-1, (self.heads, -1)).transpose(1, 2),
             (qk0, qk1, v0, v1),
         )
-        if self.flash is not None and qk0.device.type == "cuda":
-            m0 = self.flash(qk0, qk1, v1, mask)
-            m1 = self.flash(
-                qk1, qk0, v0, mask.transpose(-1, -2) if mask is not None else None
-            )
-        else:
-            qk0, qk1 = qk0 * self.scale**0.5, qk1 * self.scale**0.5
-            sim = torch.einsum("bhid, bhjd -> bhij", qk0, qk1)
-            if mask is not None:
-                sim = sim.masked_fill(~mask, -float("inf"))
-            attn01 = F.softmax(sim, dim=-1)
-            attn10 = F.softmax(sim.transpose(-2, -1).contiguous(), dim=-1)
-            m0 = torch.einsum("bhij, bhjd -> bhid", attn01, v1)
-            m1 = torch.einsum("bhji, bhjd -> bhid", attn10.transpose(-2, -1), v0)
-            if mask is not None:
-                m0, m1 = m0.nan_to_num(), m1.nan_to_num()
+        qk0, qk1 = qk0 * self.scale**0.5, qk1 * self.scale**0.5
+        sim = torch.einsum("bhid, bhjd -> bhij", qk0, qk1)
+        if mask is not None:
+            sim = sim.masked_fill(~mask, -float("inf"))
+        attn01 = F.softmax(sim, dim=-1)
+        attn10 = F.softmax(sim.transpose(-2, -1).contiguous(), dim=-1)
+        m0 = torch.einsum("bhij, bhjd -> bhid", attn01, v1)
+        m1 = torch.einsum("bhji, bhjd -> bhid", attn10.transpose(-2, -1), v0)
+        if mask is not None:
+            m0, m1 = m0.nan_to_num(), m1.nan_to_num()
         m0, m1 = self.map_(lambda t: t.transpose(1, 2).flatten(start_dim=-2), m0, m1)
         m0, m1 = self.map_(self.to_out, m0, m1)
         x0 = x0 + self.ffn(torch.cat([x0, m0], -1))
@@ -253,18 +205,44 @@ class TransformerLayer(nn.Module):
         return self.cross_attn(desc0, desc1, mask)
 
 
-def sigmoid_log_double_softmax(
-    sim: torch.Tensor, z0: torch.Tensor, z1: torch.Tensor
-) -> torch.Tensor:
+# def sigmoid_log_double_softmax(
+#     sim: torch.Tensor, z0: torch.Tensor, z1: torch.Tensor
+# ) -> torch.Tensor:
+#     """create the log assignment matrix from logits and similarity"""
+#     b, m, n = sim.shape
+#     certainties = F.logsigmoid(z0) + F.logsigmoid(z1).transpose(1, 2)
+#     scores0 = F.log_softmax(sim, 2)
+#     scores1 = F.log_softmax(sim.transpose(-1, -2).contiguous(), 2).transpose(-1, -2)
+#     scores = sim.new_full((b, m + 1, n + 1), 0)
+#     scores[:, :m, :n] = scores0 + scores1 + certainties
+#     scores[:, :-1, -1] = F.logsigmoid(-z0.squeeze(-1))
+#     scores[:, -1, :-1] = F.logsigmoid(-z1.squeeze(-1))
+#     return scores
+
+def sigmoid_log_double_softmax(sim: torch.Tensor, z0: torch.Tensor, z1: torch.Tensor) -> torch.Tensor:
     """create the log assignment matrix from logits and similarity"""
     b, m, n = sim.shape
+
+    # Calculate certainties and scores0, scores1 as before
     certainties = F.logsigmoid(z0) + F.logsigmoid(z1).transpose(1, 2)
     scores0 = F.log_softmax(sim, 2)
     scores1 = F.log_softmax(sim.transpose(-1, -2).contiguous(), 2).transpose(-1, -2)
+
+    # Create scores tensor
     scores = sim.new_full((b, m + 1, n + 1), 0)
-    scores[:, :m, :n] = scores0 + scores1 + certainties
-    scores[:, :-1, -1] = F.logsigmoid(-z0.squeeze(-1))
-    scores[:, -1, :-1] = F.logsigmoid(-z1.squeeze(-1))
+
+    # Merge the scores0, scores1, and certainties into scores without slice assignment
+    scores_main = scores0 + scores1 + certainties
+    scores[:, :m, :n] = scores_main
+
+    # Compute the scores for the last column and row
+    last_col_scores = F.logsigmoid(-z0.squeeze(-1)).unsqueeze(2)
+    last_row_scores = F.logsigmoid(-z1.squeeze(-1)).unsqueeze(1)
+
+    # Update last column and row in scores
+    scores[:, :-1, -1:] = last_col_scores
+    scores[:, -1:, :-1] = last_row_scores
+
     return scores
 
 
@@ -299,7 +277,8 @@ def filter_matches(scores: torch.Tensor, th: float):
     mutual0 = indices0 == m1.gather(1, m0)
     mutual1 = indices1 == m0.gather(1, m1)
     max0_exp = max0.values.exp()
-    zero = max0_exp.new_tensor(0)
+    #zero = max0_exp.new_tensor(0)
+    zero = torch.zeros(1, device=max0_exp.device, dtype=max0_exp.dtype)
     mscores0 = torch.where(mutual0, max0_exp, zero)
     mscores1 = torch.where(mutual1, mscores0.gather(1, m1), zero)
     valid0 = mutual0 & (mscores0 > th)
@@ -314,24 +293,12 @@ class LightGlue(nn.Module):
         "name": "lightglue",  # just for interfacing
         "input_dim": 256,  # input descriptor dimension (autoselected from weights)
         "descriptor_dim": 256,
-        "add_scale_ori": False,
-        "n_layers": 9,
+        "n_layers": 5,
         "num_heads": 4,
-        "flash": True,  # enable FlashAttention if available.
-        "mp": False,  # enable mixed precision
-        "depth_confidence": 0.95,  # early stopping, disable with -1
-        "width_confidence": 0.99,  # point pruning, disable with -1
+        "depth_confidence": 0.7,  # early stopping, disable with -1
+        "width_confidence": -1,  # point pruning, disable with -1
         "filter_threshold": 0.1,  # match threshold
         "weights": None,
-    }
-
-    # Point pruning involves an overhead (gather).
-    # Therefore, we only activate it if there are enough keypoints.
-    pruning_keypoint_thresholds = {
-        "cpu": -1,
-        "mps": -1,
-        "cuda": 1024,
-        "flash": 1536,
     }
 
     required_data_keys = ["image0", "image1"]
@@ -343,19 +310,6 @@ class LightGlue(nn.Module):
         "superpoint": {
             "weights": "superpoint_lightglue",
             "input_dim": 256,
-        },
-        "disk": {
-            "weights": "disk_lightglue",
-            "input_dim": 128,
-        },
-        "aliked": {
-            "weights": "aliked_lightglue",
-            "input_dim": 128,
-        },
-        "sift": {
-            "weights": "sift_lightglue",
-            "input_dim": 128,
-            "add_scale_ori": True,
         },
     }
 
@@ -378,13 +332,13 @@ class LightGlue(nn.Module):
 
         head_dim = conf.descriptor_dim // conf.num_heads
         self.posenc = LearnableFourierPositionalEncoding(
-            2 + 2 * self.conf.add_scale_ori, head_dim, head_dim
+            2, head_dim, head_dim
         )
 
         h, n, d = conf.num_heads, conf.n_layers, conf.descriptor_dim
 
         self.transformers = nn.ModuleList(
-            [TransformerLayer(d, h, conf.flash) for _ in range(n)]
+            [TransformerLayer(d, h) for _ in range(n)]
         )
 
         self.log_assignment = nn.ModuleList([MatchAssignment(d) for _ in range(n)])
@@ -425,11 +379,6 @@ class LightGlue(nn.Module):
     def compile(
         self, mode="reduce-overhead", static_lengths=[256, 512, 768, 1024, 1280, 1536]
     ):
-        if self.conf.width_confidence != -1:
-            warnings.warn(
-                "Point pruning is partially disabled for compiled forward.",
-                stacklevel=2,
-            )
 
         for i in range(self.conf.n_layers):
             self.transformers[i].masked_forward = torch.compile(
@@ -459,10 +408,6 @@ class LightGlue(nn.Module):
             matching_scores1: [B x N]
             matches: List[[Si x 2]], scores: List[[Si]]
         """
-        with torch.autocast(enabled=self.conf.mp, device_type="cuda"):
-            return self._forward(data)
-
-    def _forward(self, data: dict) -> dict:
         for key in self.required_data_keys:
             assert key in data, f"Missing key {key} in data"
         data0, data1 = data["image0"], data["image1"]
@@ -473,14 +418,6 @@ class LightGlue(nn.Module):
         size0, size1 = data0.get("image_size"), data1.get("image_size")
         kpts0 = normalize_keypoints(kpts0, size0).clone()
         kpts1 = normalize_keypoints(kpts1, size1).clone()
-
-        if self.conf.add_scale_ori:
-            kpts0 = torch.cat(
-                [kpts0] + [data0[k].unsqueeze(-1) for k in ("scales", "oris")], -1
-            )
-            kpts1 = torch.cat(
-                [kpts1] + [data1[k].unsqueeze(-1) for k in ("scales", "oris")], -1
-            )
         desc0 = data0["descriptors"].detach().contiguous()
         desc1 = data1["descriptors"].detach().contiguous()
 
@@ -507,43 +444,12 @@ class LightGlue(nn.Module):
         encoding1 = self.posenc(kpts1)
 
         # GNN + final_proj + assignment
-        do_early_stop = self.conf.depth_confidence > 0
-        do_point_pruning = self.conf.width_confidence > 0 and not do_compile
-        pruning_th = self.pruning_min_kpts(device)
-        if do_point_pruning:
-            ind0 = torch.arange(0, m, device=device)[None]
-            ind1 = torch.arange(0, n, device=device)[None]
-            # We store the index of the layer at which pruning is detected.
-            prune0 = torch.ones_like(ind0)
-            prune1 = torch.ones_like(ind1)
-        token0, token1 = None, None
         for i in range(self.conf.n_layers):
             desc0, desc1 = self.transformers[i](
                 desc0, desc1, encoding0, encoding1, mask0=mask0, mask1=mask1
             )
             if i == self.conf.n_layers - 1:
                 continue  # no early stopping or adaptive width at last layer
-
-            if do_early_stop:
-                token0, token1 = self.token_confidence[i](desc0, desc1)
-                if self.check_if_stop(token0[..., :m, :], token1[..., :n, :], i, m + n):
-                    break
-            if do_point_pruning and desc0.shape[-2] > pruning_th:
-                scores0 = self.log_assignment[i].get_matchability(desc0)
-                prunemask0 = self.get_pruning_mask(token0, scores0, i)
-                keep0 = torch.where(prunemask0)[1]
-                ind0 = ind0.index_select(1, keep0)
-                desc0 = desc0.index_select(1, keep0)
-                encoding0 = encoding0.index_select(-2, keep0)
-                prune0[:, ind0] += 1
-            if do_point_pruning and desc1.shape[-2] > pruning_th:
-                scores1 = self.log_assignment[i].get_matchability(desc1)
-                prunemask1 = self.get_pruning_mask(token1, scores1, i)
-                keep1 = torch.where(prunemask1)[1]
-                ind1 = ind1.index_select(1, keep1)
-                desc1 = desc1.index_select(1, keep1)
-                encoding1 = encoding1.index_select(-2, keep1)
-                prune1[:, ind1] += 1
 
         desc0, desc1 = desc0[..., :m, :], desc1[..., :n, :]
         scores, _ = self.log_assignment[i](desc0, desc1)
@@ -553,37 +459,20 @@ class LightGlue(nn.Module):
             valid = m0[k] > -1
             m_indices_0 = torch.where(valid)[0]
             m_indices_1 = m0[k][valid]
-            if do_point_pruning:
-                m_indices_0 = ind0[k, m_indices_0]
-                m_indices_1 = ind1[k, m_indices_1]
             matches.append(torch.stack([m_indices_0, m_indices_1], -1))
             mscores.append(mscores0[k][valid])
 
         # TODO: Remove when hloc switches to the compact format.
-        if do_point_pruning:
-            m0_ = torch.full((b, m), -1, device=m0.device, dtype=m0.dtype)
-            m1_ = torch.full((b, n), -1, device=m1.device, dtype=m1.dtype)
-            m0_[:, ind0] = torch.where(m0 == -1, -1, ind1.gather(1, m0.clamp(min=0)))
-            m1_[:, ind1] = torch.where(m1 == -1, -1, ind0.gather(1, m1.clamp(min=0)))
-            mscores0_ = torch.zeros((b, m), device=mscores0.device)
-            mscores1_ = torch.zeros((b, n), device=mscores1.device)
-            mscores0_[:, ind0] = mscores0
-            mscores1_[:, ind1] = mscores1
-            m0, m1, mscores0, mscores1 = m0_, m1_, mscores0_, mscores1_
-        else:
-            prune0 = torch.ones_like(mscores0) * self.conf.n_layers
-            prune1 = torch.ones_like(mscores1) * self.conf.n_layers
+        prune0 = torch.ones_like(mscores0) * self.conf.n_layers
+        prune1 = torch.ones_like(mscores1) * self.conf.n_layers
 
         pred = {
             "matches0": m0,
             "matches1": m1,
             "matching_scores0": mscores0,
             "matching_scores1": mscores1,
-            "stop": i + 1,
             "matches": matches,
             "scores": mscores,
-            "prune0": prune0,
-            "prune1": prune1,
         }
 
         return pred
@@ -592,31 +481,3 @@ class LightGlue(nn.Module):
         """scaled confidence threshold"""
         threshold = 0.8 + 0.1 * np.exp(-4.0 * layer_index / self.conf.n_layers)
         return np.clip(threshold, 0, 1)
-
-    def get_pruning_mask(
-        self, confidences: torch.Tensor, scores: torch.Tensor, layer_index: int
-    ) -> torch.Tensor:
-        """mask points which should be removed"""
-        keep = scores > (1 - self.conf.width_confidence)
-        if confidences is not None:  # Low-confidence points are never pruned.
-            keep |= confidences <= self.confidence_thresholds[layer_index]
-        return keep
-
-    def check_if_stop(
-        self,
-        confidences0: torch.Tensor,
-        confidences1: torch.Tensor,
-        layer_index: int,
-        num_points: int,
-    ) -> torch.Tensor:
-        """evaluate stopping condition"""
-        confidences = torch.cat([confidences0, confidences1], -1)
-        threshold = self.confidence_thresholds[layer_index]
-        ratio_confident = 1.0 - (confidences < threshold).float().sum() / num_points
-        return ratio_confident > self.conf.depth_confidence
-
-    def pruning_min_kpts(self, device: torch.device):
-        if self.conf.flash and FLASH_AVAILABLE and device.type == "cuda":
-            return self.pruning_keypoint_thresholds["flash"]
-        else:
-            return self.pruning_keypoint_thresholds[device.type]
